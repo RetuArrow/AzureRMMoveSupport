bc52cd6f59b6a87a32d57df60de83e64001ad983
RetuArrow <126192521+RetuArrow@users.noreply.github.com>
2023-03-24 14:52:04 +0200

    Add files via upload
    Get Azure resources from Get-AzureRmResources instead of CSV

---
+++ AzureRMMoveCheck.ps1
@@ -0,0 +1,18 @@
+$uri = "https://raw.githubusercontent.com/tfitzmac/resource-capabilities/master/move-support-resources.csv"
+Invoke-WebRequest -Uri $uri -OutFile .\moveSupported.csv
+
+$moveSupported = Import-Csv -Path .\moveSupported.csv
+#$Azureresources = Import-Csv -Path .\Azureresources.csv
+$Azureresources = Get-AzureRmResource | select ResourceId, Name, ResourceType
+"Subscription,Resource Name,Resource Type,Move Subscription" |Out-File -FilePath .\results.csv
+foreach($resource in $Azureresources){
+    foreach($support in $moveSupported){
+        $test = $support.Resource.ToLower()
+        #$resource."Resource Type" + ":" + $test #| Out-File -FilePath .\results.csv -Append
+        if($resource."ResourceType" -like "*"+$test+"*") {
+            write-host $resource.resourceid.Split("/")[2] "," $resource.name "," $support."Move Subscription"
+            $resource.resourceid.Split("/")[2]+","+$resource.name +","+$resource."ResourceType" +","+ $support."Move Subscription" | Out-File -FilePath .\results.csv -Append
+        }
+
+    }
+}
