9889327727fc1fe43df5bc0bd7890fb2a7b24b2d
RetuArrow <126192521+RetuArrow@users.noreply.github.com>
2023-04-13 11:55:07 +0300

    Update AzureRMMoveCheck.ps1
    Migrate to get-azresource, adding SKU

--- AzureRMMoveCheck.ps1
+++ AzureRMMoveCheck.ps1
@@ -3,14 +3,14 @@ Invoke-WebRequest -Uri $uri -OutFile .\moveSupported.csv
 
 $moveSupported = Import-Csv -Path .\moveSupported.csv
 #$Azureresources = Import-Csv -Path .\Azureresources.csv
-$Azureresources = Get-AzureRmResource | select ResourceId, Name, ResourceType
-"Subscription,Resource Name,Resource Type,Move Subscription" |Out-File -FilePath .\results.csv
+$Azureresources = Get-AzResource | select ResourceId, Name, ResourceType, @{Label='SKU';Expression={$_.Sku.Name}}
+"Subscription,SKU,Resource Name,Resource Type,Move Subscription" |Out-File -FilePath .\results.csv
 foreach($resource in $Azureresources){
     foreach($support in $moveSupported){
         $test = $support.Resource.ToLower()
         #$resource."Resource Type" + ":" + $test #| Out-File -FilePath .\results.csv -Append
         if($resource."ResourceType" -like "*"+$test+"*") {
-            write-host $resource.resourceid.Split("/")[2] "," $resource.name "," $support."Move Subscription"
+            write-host $resource.resourceid.Split("/")[2] "," $resource.SKU "," $resource.name "," $support."Move Subscription"
             $resource.resourceid.Split("/")[2]+","+$resource.name +","+$resource."ResourceType" +","+ $support."Move Subscription" | Out-File -FilePath .\results.csv -Append
         }
 
